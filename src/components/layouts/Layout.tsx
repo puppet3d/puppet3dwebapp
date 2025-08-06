cbbda058ef4b1ab187656790ad60b18e957f17e6
GyeongHo Kim <rlarudgh2017@gmail.com>
2025-08-06 21:46:58 +0900

    feat(SCRUM-2): modify style
    - canvas가 스크린에 꽉차게 Layout 수정

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,13 +1,14 @@
-import { Header } from "./Header";
-
 interface LayoutProps {
   children: React.ReactNode;
 }
 
+/**
+ * 모바일, 데스크탑 모두 화면 전체를 차지하고 canvas는 w-full, h-full로 만들어서 화면 전체를 차지하도록 함
+ * 추후, setting 버튼이나 채팅 input 추가시 Layout이 relative이므로 absolute로 추가하면 됨
+ */
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Header />
+    <div className="relative flex h-screen w-screen flex-col items-center justify-center">
       {children}
     </div>
   );
