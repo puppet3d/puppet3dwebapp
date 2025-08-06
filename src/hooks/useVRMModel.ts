a3c28875195d8fe5eac0ff58f65a756e58f470b6
GyeongHo Kim <rlarudgh2017@gmail.com>
2025-08-06 21:49:29 +0900

    feat(SCRUM-2): added AppCanvas, VRMModelLoader, useVRMModel
    - r3f의 Canvas 활용한 AppCanvas 추가
    - VRMModelLoader 추가, 하위 컴포넌트는 userVRMModel에서 vrmModel과 loadVRMModel을 사용할
    수 있음

---
+++ src/hooks/useVRMModel.ts
@@ -0,0 +1,19 @@
+import { VRM } from "@pixiv/three-vrm";
+import { createContext, useContext } from "react";
+
+interface VRMModelContextType {
+  vrmModel: VRM | null;
+  loadVRMModel: (url: string) => Promise<void>;
+}
+
+export const VRMModelContext = createContext<VRMModelContextType | undefined>(
+  undefined,
+);
+
+export const useVRMModel = () => {
+  const context = useContext(VRMModelContext);
+  if (context === undefined) {
+    throw new Error("useVRMModel must be used within a VRMModelProvider");
+  }
+  return context;
+};
