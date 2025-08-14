e7c27fe16857e223447f62273a241badd21089a0
GyeongHo Kim <rlarudgh2017@gmail.com>
2025-08-14 21:35:00 +0900

    feat(SCRUM-2): add fallback component
    - added lottie react animations(error, loading)
    - added CanvasFallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { AppCanvas } from "@/components/avatar/AppCanvas";
 import { VRMModelLoader } from "@/components/avatar/VRMModelLoader";
+import { CanvasFallback } from "@/components/layouts/CanvasFallback";
 import { Layout } from "@/components/layouts/Layout";
+import { ErrorBoundary } from "react-error-boundary";
+
+const EXAMPLE_VRM_URL = "/VRM1_Constraint_Twist_Sample.vrm";
 
 function App() {
   return (
     <Layout>
-      <AppCanvas>
-        <VRMModelLoader url="/VRM1_Constraint_Twist_Sample.vrm" />
-      </AppCanvas>
+      <ErrorBoundary fallback={<CanvasFallback type="error" />}>
+        <AppCanvas>
+          <VRMModelLoader url={EXAMPLE_VRM_URL} />
+        </AppCanvas>
+      </ErrorBoundary>
     </Layout>
   );
 }
