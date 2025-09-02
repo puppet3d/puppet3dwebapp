a774d474a4bac679655fbee2d0844bf64bb83f34
GyeongHo Kim <rlarudgh2017@gmail.com>
2025-09-02 21:57:16 +0900

    chore(SCRUM-13): added electron forge configs
    - electron 설정과 script들 추가
    - electron용 eslint, tsconfig 추가
    - 기본 템플릿 추가

---
+++ vite.main.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'cjs',
+        entryFileNames: '[name].cjs',
+      },
+    },
+  },
+});
